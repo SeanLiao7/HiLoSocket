8ba86ee02efaf1cc7e54f4b0ea663fe6c9d5af8b
Sean Liao <Sean_L1@ms1.hilosystems.tw>
2017-09-30 10:40:16 +0800

    1. 補足例外處理資訊。

--- src/HiLoSocket/CommandFormatter/Implements/JsonCommandFormatter.cs
+++ src/HiLoSocket/CommandFormatter/Implements/JsonCommandFormatter.cs
@@ -43,16 +43,20 @@ namespace HiLoSocket.CommandFormatter.Implements
         private static void CheckIfCanBeDeserialized( byte[ ] bytes )
         {
             if ( bytes == null )
-                throw new ArgumentNullException( nameof( bytes ), "輸入參數沒東西可以反序列化喔" );
+                throw new ArgumentNullException( nameof( bytes ),
+                    $"輸入參數沒東西可以反序列化喔，類別名稱 : {nameof( JsonCommandFormatter<TCommandModel> )}。" );
 
             if ( bytes.Length == 0 )
-                throw new ArgumentException( "資料長度不能為零啦。", nameof( bytes ) );
+                throw new ArgumentException(
+                    $"資料長度不能為零啦，類別名稱 : {nameof( JsonCommandFormatter<TCommandModel> )}。",
+                    nameof( bytes ) );
         }
 
         private static void CheckIfCanBeSerialized( TCommandModel commandModel )
         {
             if ( commandModel == null )
-                throw new ArgumentNullException( nameof( commandModel ), "輸入參數沒東西可以序列化喔。" );
+                throw new ArgumentNullException( nameof( commandModel ),
+                    $"輸入參數沒東西可以序列化喔，類別名稱 : {nameof( JsonCommandFormatter<TCommandModel> )}。" );
         }
     }
 }
--- src/HiLoSocket/CommandFormatter/Implements/ProtobufCommandFormatter.cs
+++ src/HiLoSocket/CommandFormatter/Implements/ProtobufCommandFormatter.cs
@@ -49,16 +49,20 @@ namespace HiLoSocket.CommandFormatter.Implements
         private static void CheckIfCanBeDeserialized( byte[ ] bytes )
         {
             if ( bytes == null )
-                throw new ArgumentNullException( nameof( bytes ), "輸入參數沒東西可以反序列化喔" );
+                throw new ArgumentNullException( nameof( bytes ),
+                    $"輸入參數沒東西可以反序列化喔，類別名稱 : {nameof( ProtobufCommandFormatter<TCommandModel> )}。" );
 
             if ( bytes.Length == 0 )
-                throw new ArgumentException( "資料長度不能為零啦。", nameof( bytes ) );
+                throw new ArgumentException(
+                    $"資料長度不能為零啦，類別名稱 : {nameof( ProtobufCommandFormatter<TCommandModel> )}。",
+                    nameof( bytes ) );
         }
 
         private static void CheckIfCanBeSerialized( TCommandModel commandModel )
         {
             if ( commandModel == null )
-                throw new ArgumentNullException( nameof( commandModel ), "輸入參數沒東西可以序列化喔。" );
+                throw new ArgumentNullException( nameof( commandModel ),
+                    $"輸入參數沒東西可以序列化喔，類別名稱 : {nameof( ProtobufCommandFormatter<TCommandModel> )}。" );
         }
     }
 }
