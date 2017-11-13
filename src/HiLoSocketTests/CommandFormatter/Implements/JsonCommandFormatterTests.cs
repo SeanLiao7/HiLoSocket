e540f23d5afd85d2277886de0380265a81623b19
Sean Liao <Sean_L1@ms1.hilosystems.tw>
2017-11-13 15:18:53 +0800

    1. 利用 Shouldly 重新命名。 2. 測試方法重新命名。

--- src/HiLoSocketTests/CommandFormatter/Implements/JsonCommandFormatterTests.cs
+++ src/HiLoSocketTests/CommandFormatter/Implements/JsonCommandFormatterTests.cs
@@ -10,35 +10,35 @@ namespace HiLoSocketTests.CommandFormatter.Implements
     public class JsonCommandFormatterTests
     {
         [Test]
-        public void DeserializeNullInputTest( )
+        public void Deserialize_NullInput_ThrowsArgumentNullException( )
         {
-            var formatter = FormatterFactory<JsonData>.CreateFormatter( FormatterType.JSonFormatter );
+            var formatter = FormatterFactory<JsonDataObject>.CreateFormatter( FormatterType.JSonFormatter );
             Should.Throw<ArgumentNullException>(
                 ( ) => formatter.Deserialize( null ) );
         }
 
         [Test]
-        public void DeserializeZeroLengthTest( )
+        public void Deserialize_ZeroLengthInput_ThrowsArgumentException( )
         {
-            var formatter = FormatterFactory<JsonData>.CreateFormatter( FormatterType.JSonFormatter );
+            var formatter = FormatterFactory<JsonDataObject>.CreateFormatter( FormatterType.JSonFormatter );
             var input = new byte[ 0 ];
             Should.Throw<ArgumentException>(
                 ( ) => formatter.Deserialize( input ) );
         }
 
         [Test]
-        public void SerializeNullInputTest( )
+        public void Serialize_NullInput_ThrowsArgumentNullException( )
         {
-            var formatter = FormatterFactory<JsonData>.CreateFormatter( FormatterType.JSonFormatter );
+            var formatter = FormatterFactory<JsonDataObject>.CreateFormatter( FormatterType.JSonFormatter );
             Should.Throw<ArgumentNullException>(
                 ( ) => formatter.Serialize( null ) );
         }
 
         [Test]
-        public void SerializeObjectTest( )
+        public void SerializeAndDeSerialze_JsonDataObject_ShouldBeEqual( )
         {
-            var formatter = FormatterFactory<JsonData>.CreateFormatter( FormatterType.JSonFormatter );
-            var expected = new JsonData( Guid.NewGuid( ) )
+            var formatter = FormatterFactory<JsonDataObject>.CreateFormatter( FormatterType.JSonFormatter );
+            var expected = new JsonDataObject( Guid.NewGuid( ) )
             {
                 Name = "Penny",
                 Age = 20
@@ -51,56 +51,56 @@ namespace HiLoSocketTests.CommandFormatter.Implements
         [TestCase( "*測試 Test#_$% ?" )]
         [TestCase( "*測試 0    ZZp $% ? ●" )]
         [TestCase( "* 測 → 】試 0 『 Y Z　＠ ●" )]
-        public void SerializeStringTest( string expected )
+        public void SerializeAndDeSerialze_String_ShouldBeEqual( string expected )
         {
             var formatter = FormatterFactory<string>.CreateFormatter( FormatterType.JSonFormatter );
             var serializedResult = formatter.Serialize( expected );
             var actual = formatter.Deserialize( serializedResult );
             actual.ShouldBe( expected );
         }
-    }
 
-    public class JsonData : IEquatable<JsonData>
-    {
-        public int Age { get; set; }
-        public Guid Id { get; }
-        public string Name { get; set; }
-
-        public JsonData( Guid id )
+        private class JsonDataObject : IEquatable<JsonDataObject>
         {
-            Id = id;
-        }
+            public int Age { get; set; }
+            public Guid Id { get; }
+            public string Name { get; set; }
 
-        public static bool operator !=( JsonData a, JsonData b )
-        {
-            return !( a == b );
-        }
+            public JsonDataObject( Guid id )
+            {
+                Id = id;
+            }
 
-        public static bool operator ==( JsonData a, JsonData b )
-        {
-            return Equals( a, b );
-        }
+            public static bool operator !=( JsonDataObject a, JsonDataObject b )
+            {
+                return !( a == b );
+            }
 
-        public bool Equals( JsonData other )
-        {
-            if ( ReferenceEquals( null, other ) ) return false;
-            if ( ReferenceEquals( this, other ) ) return true;
-            return Id.Equals( other.Id )
-                   && Name.Equals( other.Name )
-                   && Age.Equals( other.Age );
-        }
+            public static bool operator ==( JsonDataObject a, JsonDataObject b )
+            {
+                return Equals( a, b );
+            }
 
-        public override bool Equals( object obj )
-        {
-            if ( ReferenceEquals( null, obj ) ) return false;
-            if ( ReferenceEquals( this, obj ) ) return true;
-            return obj.GetType( ) == GetType( )
-                   && Equals( ( JsonData ) obj );
-        }
+            public bool Equals( JsonDataObject other )
+            {
+                if ( ReferenceEquals( null, other ) ) return false;
+                if ( ReferenceEquals( this, other ) ) return true;
+                return Id.Equals( other.Id )
+                       && Name.Equals( other.Name )
+                       && Age.Equals( other.Age );
+            }
 
-        public override int GetHashCode( )
-        {
-            return Id.GetHashCode( );
+            public override bool Equals( object obj )
+            {
+                if ( ReferenceEquals( null, obj ) ) return false;
+                if ( ReferenceEquals( this, obj ) ) return true;
+                return obj.GetType( ) == GetType( )
+                       && Equals( ( JsonDataObject ) obj );
+            }
+
+            public override int GetHashCode( )
+            {
+                return Id.GetHashCode( );
+            }
         }
     }
 }
--- src/HiLoSocketTests/CommandFormatter/Implements/MessagePackCommandFormatterTests.cs
+++ src/HiLoSocketTests/CommandFormatter/Implements/MessagePackCommandFormatterTests.cs
@@ -10,35 +10,35 @@ namespace HiLoSocketTests.CommandFormatter.Implements
     public class MessagePackCommandFormatterTests
     {
         [Test]
-        public void DeserializeNullInputTest( )
+        public void Deserialize_NullInput_ThrowsArgumentNullException( )
         {
-            var formatter = FormatterFactory<MessagePackData>.CreateFormatter( FormatterType.MessagePackFormatter );
+            var formatter = FormatterFactory<MessagePackDataObject>.CreateFormatter( FormatterType.MessagePackFormatter );
             Should.Throw<ArgumentNullException>(
                 ( ) => formatter.Deserialize( null ) );
         }
 
         [Test]
-        public void DeserializeZeroLengthTest( )
+        public void Deserialize_ZeroLengthInput_ThrowsArgumentException( )
         {
-            var formatter = FormatterFactory<MessagePackData>.CreateFormatter( FormatterType.MessagePackFormatter );
+            var formatter = FormatterFactory<MessagePackDataObject>.CreateFormatter( FormatterType.MessagePackFormatter );
             var input = new byte[ 0 ];
             Should.Throw<ArgumentException>(
                 ( ) => formatter.Deserialize( input ) );
         }
 
         [Test]
-        public void SerializeNullInputTest( )
+        public void Serialize_NullInput_ThrowsArgumentNullException( )
         {
-            var formatter = FormatterFactory<MessagePackData>.CreateFormatter( FormatterType.MessagePackFormatter );
+            var formatter = FormatterFactory<MessagePackDataObject>.CreateFormatter( FormatterType.MessagePackFormatter );
             Should.Throw<ArgumentNullException>(
                 ( ) => formatter.Serialize( null ) );
         }
 
         [Test]
-        public void SerializeObjectTest( )
+        public void SerializeAndDeserialize_MessagePackDataObject_ShouldBeEqual( )
         {
-            var formatter = FormatterFactory<MessagePackData>.CreateFormatter( FormatterType.MessagePackFormatter );
-            var expected = new MessagePackData( Guid.NewGuid( ) )
+            var formatter = FormatterFactory<MessagePackDataObject>.CreateFormatter( FormatterType.MessagePackFormatter );
+            var expected = new MessagePackDataObject( Guid.NewGuid( ) )
             {
                 Name = "Penny",
                 Age = 20
@@ -51,56 +51,56 @@ namespace HiLoSocketTests.CommandFormatter.Implements
         [TestCase( "*測試 Test#_$% ?" )]
         [TestCase( "*測試 0    ZZp $% ? ●" )]
         [TestCase( "* 測 → 】試 0 『 Y Z　＠ ●" )]
-        public void SerializeStringTest( string expected )
+        public void SerializeAndDeserialize_String_ShouldBeEqual( string expected )
         {
             var formatter = FormatterFactory<string>.CreateFormatter( FormatterType.MessagePackFormatter );
             var serializedResult = formatter.Serialize( expected );
             var actual = formatter.Deserialize( serializedResult );
             actual.ShouldBe( expected );
         }
-    }
 
-    public class MessagePackData : IEquatable<MessagePackData>
-    {
-        public int Age { get; set; }
-        public Guid Id { get; }
-        public string Name { get; set; }
-
-        public MessagePackData( Guid id )
+        public class MessagePackDataObject : IEquatable<MessagePackDataObject>
         {
-            Id = id;
-        }
+            public int Age { get; set; }
+            public Guid Id { get; }
+            public string Name { get; set; }
 
-        public static bool operator !=( MessagePackData a, MessagePackData b )
-        {
-            return !( a == b );
-        }
+            public MessagePackDataObject( Guid id )
+            {
+                Id = id;
+            }
 
-        public static bool operator ==( MessagePackData a, MessagePackData b )
-        {
-            return Equals( a, b );
-        }
+            public static bool operator !=( MessagePackDataObject a, MessagePackDataObject b )
+            {
+                return !( a == b );
+            }
 
-        public bool Equals( MessagePackData other )
-        {
-            if ( ReferenceEquals( null, other ) ) return false;
-            if ( ReferenceEquals( this, other ) ) return true;
-            return Id.Equals( other.Id )
-                   && Name.Equals( other.Name )
-                   && Age.Equals( other.Age );
-        }
+            public static bool operator ==( MessagePackDataObject a, MessagePackDataObject b )
+            {
+                return Equals( a, b );
+            }
 
-        public override bool Equals( object obj )
-        {
-            if ( ReferenceEquals( null, obj ) ) return false;
-            if ( ReferenceEquals( this, obj ) ) return true;
-            return obj.GetType( ) == GetType( )
-                   && Equals( ( MessagePackData ) obj );
-        }
+            public bool Equals( MessagePackDataObject other )
+            {
+                if ( ReferenceEquals( null, other ) ) return false;
+                if ( ReferenceEquals( this, other ) ) return true;
+                return Id.Equals( other.Id )
+                       && Name.Equals( other.Name )
+                       && Age.Equals( other.Age );
+            }
 
-        public override int GetHashCode( )
-        {
-            return Id.GetHashCode( );
+            public override bool Equals( object obj )
+            {
+                if ( ReferenceEquals( null, obj ) ) return false;
+                if ( ReferenceEquals( this, obj ) ) return true;
+                return obj.GetType( ) == GetType( )
+                       && Equals( ( MessagePackDataObject ) obj );
+            }
+
+            public override int GetHashCode( )
+            {
+                return Id.GetHashCode( );
+            }
         }
     }
 }
